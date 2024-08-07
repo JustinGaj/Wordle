95a43df1e9a986dccea8312f702fe9083f08b8a2
JustinGaj <jtg0825@yahoo.com>
2024-08-06 23:25:06 -0400

    initial

---
+++ play_wordle.py
@@ -0,0 +1,16 @@
+from wordle import Wordle
+
+def main():
+   print("Hello Wordle!")
+   wordle = Wordle("APPLE")
+
+   while True:
+      x = input("Type your guess: ")
+      if x == wordle.secret:
+         print("You have guessed the word!")
+         break
+      print("Your guess is incorrect!")
+
+
+if __name__ == "__main__":
+   main()
---
+++ wordle.py
@@ -0,0 +1,17 @@
+class Wordle:
+
+    MAX_ATTEMPTS = 6
+    WORD_LENGTH = 5
+
+    def __init__(self, secret: str):
+        self.secret: str = secret
+        self.attempts = []
+        pass
+
+    @property
+    def is_solved(self):
+        return self.attempts[-1] == self.secret
+    
+    @property
+    def can_attempt(self):
+        pass
