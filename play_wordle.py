e4ed5872de16170a8ec95b3ef101125e6614490d
JustinGaj <jtg0825@yahoo.com>
2024-08-07 15:15:55 -0400

    initial

--- play_wordle.py
+++ play_wordle.py
@@ -1,15 +1,24 @@
 from wordle import Wordle
+from colorama import Fore
 
 def main():
-   print("Hello Wordle!")
-   wordle = Wordle("APPLE")
+    print("Hello Wordle!")
+    wordle = Wordle("APPLE")
+    while wordle.can_attempt:
+        x = input("Type your guess: ")
 
-   while True:
-      x = input("Type your guess: ")
-      if x == wordle.secret:
-         print("You have guessed the word!")
-         break
-      print("Your guess is incorrect!")
+        if len(x) != wordle.WORD_LENGTH:
+            print(Fore.RED + f"Word must be {wordle.WORD_LENGTH} characters long.")
+            continue
+        
+        wordle.attempt(x)
+        result = wordle.guess(x)
+        print(*result, sep="\n")
+
+    if wordle.is_solved:
+        print("You've solved the puzzle!")
+    else:
+        print("You've failed to solve the puzzle!")
 
 
 if __name__ == "__main__":
--- wordle.py
+++ wordle.py
@@ -1,17 +1,39 @@
+from letter_state import LetterState
+
 class Wordle:
 
     MAX_ATTEMPTS = 6
     WORD_LENGTH = 5
 
     def __init__(self, secret: str):
-        self.secret: str = secret
+        self.secret: str = secret.upper()
         self.attempts = []
-        pass
+
+    def attempt(self, word: str):
+        word = word.upper()
+        self.attempts.append(word)
+
+    def guess(self, word: str):
+        word = word.upper()
+        result = []
+
+        for i in range(self.WORD_LENGTH):
+            char = word[i]
+            letter = LetterState(char)
+            letter.is_in_word = char in self.secret
+            letter.is_in_position = char == self.secret[i]
+            result.append(letter)
+
+        return result
 
     @property
     def is_solved(self):
-        return self.attempts[-1] == self.secret
+        return len(self.attempts) > 0 and self.attempts[-1] == self.secret
     
     @property
+    def remaining_attempts(self) -> int:
+        return self.MAX_ATTEMPTS - len(self.attempts)
+
+    @property
     def can_attempt(self):
-        pass
+        return self.remaining_attempts > 0 and not self.is_solved
