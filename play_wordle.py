62167fbb528dad30bb00eaa2dcbb3014f0ca800e
JustinGaj <jtg0825@yahoo.com>
2024-08-07 15:25:16 -0400

    initial

--- play_wordle.py
+++ play_wordle.py
@@ -8,17 +8,19 @@ def main():
         x = input("Type your guess: ")
 
         if len(x) != wordle.WORD_LENGTH:
-            print(Fore.RED + f"Word must be {wordle.WORD_LENGTH} characters long.")
+            print(Fore.RED + f"Word must be {wordle.WORD_LENGTH} characters long." + Fore.RESET)
             continue
         
         wordle.attempt(x)
-        result = wordle.guess(x)
-        print(*result, sep="\n")
+        display_results(wordle)
 
     if wordle.is_solved:
         print("You've solved the puzzle!")
     else:
         print("You've failed to solve the puzzle!")
+
+def display_results(wordle: Wordle):
+    pass
 
 
 if __name__ == "__main__":
